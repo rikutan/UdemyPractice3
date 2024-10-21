2a995cf7d3f874b30e1a8d106cfb76db221b960c
rikutan <163800982+rikutan@users.noreply.github.com>
2024-10-21 21:44:23 +0900

    delete Greeting関数の削除

--- app/src/main/java/com/example/udemypractice3/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice3/MainActivity.kt
@@ -19,29 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UdemyPractice3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UdemyPractice3Theme {
-        Greeting("Android")
+        
     }
 }
