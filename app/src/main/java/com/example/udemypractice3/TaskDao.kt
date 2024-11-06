8aae9d5ccd4ebeef9264b2271d912a1263e5fd65
rikutan <163800982+rikutan@users.noreply.github.com>
2024-11-06 14:21:09 +0900

    add DAOメソッドの非同期化

--- app/src/main/java/com/example/udemypractice3/TaskDao.kt
+++ app/src/main/java/com/example/udemypractice3/TaskDao.kt
@@ -5,24 +5,26 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
+    // 非同期ワンショットクエリ：suspend を fun の前に付ける
 
     // データベースに保存するメソッドだと認識させる
     @Insert
-    fun insertTask(task: Task)
+    suspend fun insertTask(task: Task)
 
     // DB からデータを全件取得するメソッド
     @Query("select * from Task")
-    fun loadAllTasks(): List<Task>
+    fun loadAllTasks(): Flow<List<Task>>
 
     // DB に保存されているタスクを更新する
     @Update
-    fun updateTask(task: Task)
+    suspend fun updateTask(task: Task)
 
     // DB からタスクを削除する
     @Delete
-    fun deleteTask(task: Task)
+    suspend fun deleteTask(task: Task)
 
 }
