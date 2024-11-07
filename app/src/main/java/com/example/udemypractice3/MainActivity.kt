24dccb41fcc0c846e8ea7b5b1264c43be4af7b29
rikutan <163800982+rikutan@users.noreply.github.com>
2024-11-07 14:30:09 +0900

    add FABの追加

--- app/src/main/java/com/example/udemypractice3/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice3/MainActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.udemypractice3.ui.theme.UdemyPractice3Theme
+import com.example.udemypractice3.ui.theme.Purple40
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,15 +23,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UdemyPractice3Theme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Purple40
+            ) {
+                MainContent()
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+
+    // アプリ画面の基本レイアウトを作成するための構造。
+    // トップバーやボトムバー、メインコンテンツ、FABなどを簡単に配置できる。
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainPreview() {
-    UdemyPractice3Theme {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Purple40
+    ) {
+        MainContent()
     }
 }
