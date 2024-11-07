08f98ac8214b826f7853ae251d6ed3d1c23bde10
rikutan <163800982+rikutan@users.noreply.github.com>
2024-11-07 14:06:18 +0900

    add Hiltアプリケーションとエントリーポイントの設置

--- app/src/main/java/com/example/udemypractice3/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice3/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.udemypractice3.ui.theme.UdemyPractice3Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
